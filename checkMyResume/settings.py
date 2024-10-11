238136ccb5955ab664fd3b00e3cb2e51dcf91337
Likhith Panigrahi <LikhithDeveloper>
2024-10-11 22:08:02 +0530

    Host it

--- checkMyResume/settings.py
+++ checkMyResume/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-%f16qoio1zy9y+0&o!p029*s%e*(iy129b7__2rzdsn!nz^_w0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['52.23.151.178']
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
